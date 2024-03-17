fdee967e7fd220ceb6215576254f42470f6b0653
Shaahid <138685649+shaah1d@users.noreply.github.com>
2024-03-17 11:34:05 -0700

    Update index.js

--- index.js
+++ index.js
@@ -55,18 +55,8 @@ app.patch('/comments/:id', (req,res) => {
     const foundComment = comments.find(c=> c.id === (id))
     const newCommentText = req.body.comment;
     foundComment.comment = newCommentText;
-
+    res.redirect('/comments');
 })
-// app.get('/tacos', (req,res) => {
-//     res.send("GET /tacos response");
-
-// })
-// app.post('/tacos', (req,res) => {
-//     const { meat, qty} = req.body;
-
-//     res.send(`OK, here is your ${qty} ${meat} tacos`);
-    
-// })
 app.listen(3000, () => {
     console.log("ON PORT 3000");
 })
